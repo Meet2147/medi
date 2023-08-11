5871b0d27d8e1fdec6e8546fa2d421e2ad11c8a9
Tina Jethwa <rathodtinak@gmail.com>
2023-08-12 01:11:16 +0530

    added doctor api files

--- API/get_doctor_list.py
+++ API/get_doctor_list.py
@@ -5,9 +5,3 @@ import mysql.connector
 app = FastAPI()
 
 # Database configuration
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "1234",
-    "database": "medilink",
-}
